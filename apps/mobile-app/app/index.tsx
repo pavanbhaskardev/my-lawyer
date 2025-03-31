f13e7c12ea53262e149781ab239f5ee35b09a54a
pavanbhaskar <pavanbhaskardev@gmail.com>
2025-03-31 15:54:03 +0530

    chore: installed expo-mono repo with nativewind

---
+++ apps/mobile-app/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const IndexPage = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-300">
+      <Text className="text-4xl">🔥Damn Son!</Text>
+    </View>
+  )
+}
+
+export default IndexPage
