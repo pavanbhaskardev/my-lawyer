6a543a3e5fc1d5650882a9384ca03f16622e2404
pavanbhaskar <pavanbhaskardev@gmail.com>
2025-04-12 18:02:51 +0530

    chore: added cors

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -2,30 +2,38 @@ import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { zValidator } from '@hono/zod-validator'
 import { z } from 'zod'
+import { cors } from 'hono/cors'
 
-const app = new Hono().post(
-  '/',
-  zValidator(
-    'json',
-    z.object({
-      name: z.string(),
+const app = new Hono()
+  .use('*', logger())
+  .use(
+    '*',
+    cors({
+      origin: '*',
+      credentials: true,
     })
-  ),
-  (c) => {
-    const validated = c.req.valid('json')
+  )
+  .post(
+    '/',
+    zValidator(
+      'json',
+      z.object({
+        name: z.string(),
+      })
+    ),
+    (c) => {
+      const validated = c.req.valid('json')
 
-    return c.json(
-      {
-        message: `Hello ${validated.name}`,
-      },
-      {
-        status: 200,
-      }
-    )
-  }
-)
-
-app.use('*', logger())
+      return c.json(
+        {
+          message: `Hello ${validated.name}`,
+        },
+        {
+          status: 200,
+        }
+      )
+    }
+  )
 
 export default app
 export type AppType = typeof app
