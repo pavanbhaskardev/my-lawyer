11d57e1a86cac7bfafd6667db16adfb6d386f8bb
pavanbhaskar <pavanbhaskardev@gmail.com>
2025-04-19 19:46:28 +0530

    chore: add dashboard page

---
+++ apps/mobile-app/app/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const IndexPage = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-4xl text-primary">Dashboard</Text>
+    </View>
+  )
+}
+
+export default IndexPage
